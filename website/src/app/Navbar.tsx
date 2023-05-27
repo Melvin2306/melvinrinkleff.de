f8f7d7f74686e9261567ce6bfad3746375c9d7a5
Melvin2306 <melvin.rinkleff@gmx.de>
2023-05-27 17:55:31 +0200

    added links to navbar

--- website/src/app/Navbar.tsx
+++ website/src/app/Navbar.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
+import Link from "next/link";
 
 const navigation = [
   { name: "CV", href: "cv" },
@@ -21,7 +22,7 @@ export default function Navbar() {
       aria-label="Navigation Bar"
     >
       <div className="flex lg:flex-1">
-        <a href="#" className="-m-1.5 p-1.5">
+        <Link href="#" className="-m-1.5 p-1.5">
           <span className="sr-only">Melvin Rinkleff</span>
           {/* <Image
                 className="h-8 w-auto"
@@ -30,7 +31,7 @@ export default function Navbar() {
                 width={32}
                 height={32}
               /> */}
-        </a>
+        </Link>
       </div>
       <div className="flex lg:hidden">
         <button
@@ -44,13 +45,13 @@ export default function Navbar() {
       </div>
       <div className="hidden lg:flex lg:gap-x-12">
         {navigation.map((item) => (
-          <a
+          <Link
             key={item.name}
             href={item.href}
             className="text-sm font-semibold leading-6 text-gray-900"
           >
             {item.name}
-          </a>
+          </Link>
         ))}
       </div>
     </nav>
@@ -63,7 +64,7 @@ export default function Navbar() {
       <div className="fixed inset-0 z-50" />
       <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
         <div className="flex items-center justify-between">
-          <a href="#" className="-m-1.5 p-1.5">
+          <Link href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Melvin Rinkleff</span>
             {/* <Image
                   className="h-8 w-auto"
@@ -72,7 +73,7 @@ export default function Navbar() {
                   width={32}
                   height={32}
                 /> */}
-          </a>
+          </Link>
           <button
             type="button"
             className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -86,13 +87,13 @@ export default function Navbar() {
           <div className="-my-6 divide-y divide-gray-500/10">
             <div className="space-y-2 py-6">
               {navigation.map((item) => (
-                <a
+                <Link
                   key={item.name}
                   href={item.href}
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   {item.name}
-                </a>
+                </Link>
               ))}
             </div>
           </div>
