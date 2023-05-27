decfdc5de9278c9edda69fcf428fbb1c7e8038a5
Melvin2306 <melvin.rinkleff@gmx.de>
2023-05-27 13:27:24 +0200

    Squashed commit of the following:
    commit ba964ea05efc10be28556faccf3f99c281fe0e9c
    Author: Melvin2306 <melvin.rinkleff@gmx.de>
    Date:   Sat May 27 13:25:17 2023 +0200
    
        feat: added vercel analytics to project
    
    commit 8a20a406faa4b6ee35ab744291f7250b76a4771b
    Author: Melvin2306 <melvin.rinkleff@gmx.de>
    Date:   Sat May 27 13:18:10 2023 +0200
    
        fix: deleted pages for now
    
    commit 3785c693b9adbc17113dcf2723bb2efe7a0f62ad
    Merge: c0b32b8 57110c0
    Author: Melvin Rinkleff <91799765+Melvin2306@users.noreply.github.com>
    Date:   Sat May 27 13:09:45 2023 +0200
    
        Merge pull request #3 from Melvin2306/develop
    
        First PR to test Vercel Deployment

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -1,50 +1,52 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Melvin Rinkleff',
-  description: 'Website of Melvin Rinkleff',
-  url: 'https://melvinrinkleff.de',
+  title: "Melvin Rinkleff",
+  description: "Website of Melvin Rinkleff",
+  url: "https://melvinrinkleff.de",
   keywords: [
-    'Melvin Rinkleff',
-    'Melvin',
-    'Rinkleff',
-    'melvinrinkleff',
-    'melvin rinkleff',
-    'melvinrinkleff.de',
-    'melvinrinkleff.com',
-    'melvinrinkleff.dev',
-    'melvinrinkleff.io',
-    'melvinrinkleff.net',
-    'melvinrinkleff.org',
-    'melvinrinkleff.xyz',
-    'melvinrinkleff.website',
-    'melvinrinkleff.app',
-    'melvinrinkleff.blog',
-    'melvinrinkleff.design',
-    'melvinrinkleff.info',
-    'melvinrinkleff.me',
-    'melvinrinkleff.shop',
-    'melvinrinkleff.store',
-    'melvinrinkleff.tech',
-    'melvinrinkleff.work',
-    'melvinrinkleff.live',
-    'melvinrinkleff.online',
-    'melvinrinkleff.space',
-    'melvinrinkleff.world',
+    "Melvin Rinkleff",
+    "Melvin",
+    "Rinkleff",
+    "melvinrinkleff",
+    "melvin rinkleff",
+    "melvinrinkleff.de",
+    "melvinrinkleff.com",
+    "melvinrinkleff.dev",
+    "melvinrinkleff.io",
+    "melvinrinkleff.net",
+    "melvinrinkleff.org",
+    "melvinrinkleff.xyz",
+    "melvinrinkleff.website",
+    "melvinrinkleff.app",
+    "melvinrinkleff.blog",
+    "melvinrinkleff.design",
+    "melvinrinkleff.info",
+    "melvinrinkleff.me",
+    "melvinrinkleff.shop",
+    "melvinrinkleff.store",
+    "melvinrinkleff.tech",
+    "melvinrinkleff.work",
+    "melvinrinkleff.live",
+    "melvinrinkleff.online",
+    "melvinrinkleff.space",
+    "melvinrinkleff.world",
   ],
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
-  )
+  );
 }
