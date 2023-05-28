9800bf92d91b46f6b2434f125c82606cc9298f09
Melvin2306 <melvin.rinkleff@gmx.de>
2023-05-28 12:48:39 +0200

    imprint

--- website/src/app/imprint/page.tsx
+++ website/src/app/imprint/page.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 export default function Imprint() {
   return (
     <div>
       <h1>Imprint</h1>
+      <h2> Melvin Rinkleff </h2>
+      <h3> Address </h3>
+      <p> Kornradenstraße 2A </p>
+      <p> 30419 Hannover</p>
+      <p> Germany </p>
+      <p>
+        {" "}
+        Email:{" "}
+        <Link href="mailto:info@melvinrinkleff.de">info@melvinrinkleff.de</Link>
+      </p>{" "}
     </div>
   );
 }
