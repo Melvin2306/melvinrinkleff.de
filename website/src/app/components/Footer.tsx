957085230e242f5914fcdc32bfb69f4a2f6abde1
Melvin2306 <melvin.rinkleff@gmx.de>
2023-05-28 12:48:20 +0200

    added footer

--- website/src/app/components/Footer.tsx
+++ website/src/app/components/Footer.tsx
@@ -1,24 +1,59 @@
 "use client";
-
-const navigation = [{ name: "Imprint", href: "imprint" }];
+import Image from "next/image";
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
+  const navigation = [{ name: "Imprint", href: "imprint" }];
+
+  const socials = [
+    {
+      pic: "/github.png",
+      href: "https://github.com/Melvin2306",
+    },
+    {
+      pic: "/linkedin.png",
+      href: "https://www.linkedin.com/in/melvin-rinkleff/",
+    },
+    {
+      pic: "/mail.png",
+      href: "mailto:info@melvinrinkleff.de",
+    },
+  ];
+
   return (
-    <footer
-      className="bg-gray-800flex items-center justify-between p-6 lg:px-8"
-      aria-label="Footer"
-    >
-      <div className="flex lg:gap-x-12">
-        {navigation.map((item) => (
-          <a
-            key={item.name}
-            href={item.href}
-            className="text-sm font-semibold leading-6 text-gray-900"
-          >
-            {item.name}
-          </a>
-        ))}
-      </div>
-    </footer>
+    <div className="flex flex-col h-screen">
+      <div className="flex-1"></div>
+      <footer
+        className="flex items-center justify-between pb-3"
+        aria-label="Footer"
+      >
+        <p className="text-sm font-semibold leading-6 text-gray-900">
+          © {currentYear} Melvin Rinkleff
+        </p>
+        <div>
+          {navigation.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
+        <div className="flex justify-end items-center gap-x-4">
+          {socials.map((item) => (
+            <a
+              key={item.href}
+              href={item.href}
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              <Image src={item.pic} alt={item.href} height={25} width={25} />
+            </a>
+          ))}
+        </div>
+      </footer>
+    </div>
   );
 }
