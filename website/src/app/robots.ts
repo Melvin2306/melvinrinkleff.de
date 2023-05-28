7d6fbe3858f770cb7c450ef0bfbca0294a1c54bc
Melvin2306 <melvin.rinkleff@gmx.de>
2023-05-28 13:11:37 +0200

    Update robots.ts

--- website/src/app/robots.ts
+++ website/src/app/robots.ts
@@ -5,7 +5,6 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      ignore: ["/loading", "/not-found"],
     },
     sitemap: "https://melvinrinkleff.de/sitemap.xml",
   };
