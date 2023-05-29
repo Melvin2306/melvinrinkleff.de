7035972ddc23db8326ff13288da7bf0e37695a7d
Melvin2306 <melvin.rinkleff@gmx.de>
2023-05-29 13:37:27 +0200

    Update page.tsx

--- website/src/app/contact/page.tsx
+++ website/src/app/contact/page.tsx
@@ -28,10 +28,12 @@ export default function Contact() {
         },
         body: JSON.stringify(formState),
       });
+
       console.log(formState);
       console.log(res);
 
       setFormState({ name: "", email: "", message: "" });
+
       toast.success("Message sent", {
         position: "top-right",
         autoClose: 5000,
@@ -43,7 +45,7 @@ export default function Contact() {
         theme: "light",
       });
     } catch (error) {
-      toast.error(error, {
+      toast.error("Error while sending message", {
         position: "top-right",
         autoClose: 5000,
         hideProgressBar: false,
@@ -57,33 +59,47 @@ export default function Contact() {
   };
 
   return (
-    <form onSubmit={sendEmail}>
-      <input
-        type="text"
-        name="name"
-        placeholder="Your name"
-        value={formState.name}
-        onChange={(e) => setFormState({ ...formState, name: e.target.value })}
-        required
-      />
-      <input
-        type="email"
-        name="email"
-        placeholder="Your email"
-        value={formState.email}
-        onChange={(e) => setFormState({ ...formState, email: e.target.value })}
-        required
-      />
-      <textarea
-        name="message"
-        placeholder="Your message"
-        value={formState.message}
-        onChange={(e) =>
-          setFormState({ ...formState, message: e.target.value })
-        }
-        required
-      />
-      <button type="submit">Send</button>
+    <form onSubmit={sendEmail} className="ml-4">
+      <div className="form-group mt-4 bg-lightblue-100 rounded-md py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500">
+        <input
+          type="text"
+          name="name"
+          placeholder="Your name"
+          value={formState.name}
+          onChange={(e) => setFormState({ ...formState, name: e.target.value })}
+        />
+      </div>
+      <div className="form-group mt-4 bg-lightblue-100 rounded-md py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500">
+        <input
+          type="email"
+          name="email"
+          placeholder="Your email"
+          value={formState.email}
+          onChange={(e) =>
+            setFormState({ ...formState, email: e.target.value })
+          }
+          required
+        />
+      </div>
+      <div className="form-group mt-4 bg-lightblue-100 rounded-md py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500">
+        <textarea
+          name="message"
+          placeholder="Your message"
+          value={formState.message}
+          onChange={(e) =>
+            setFormState({ ...formState, message: e.target.value })
+          }
+          required
+        />
+      </div>
+      <div className="form-group mt-4">
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Send
+        </button>
+      </div>
       <ToastContainer />
     </form>
   );
