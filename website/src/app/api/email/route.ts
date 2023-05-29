d3e66555808e52f89fc019a46612d8db02a1797d
Melvin2306 <melvin.rinkleff@gmx.de>
2023-05-29 13:37:24 +0200

    Update route.ts

--- website/src/app/api/email/route.ts
+++ website/src/app/api/email/route.ts
@@ -1,52 +1,50 @@
-// pages/api/email.ts
-
-import type { NextApiRequest, NextApiResponse } from "next";
 import nodemailer from "nodemailer";
 import validator from "validator";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextApiRequest) {
-  let {
-    name,
-    email,
-    message,
-  }: { name: string; email: string; message: string } = req.body;
-
-  // Sanitize input
+export async function POST(req: NextRequest) {
+  // let {
+  //   name,
+  //   email,
+  //   message,
+  // }: { name: string; email: string; message: string } = req.body;
+  // console.log(req.body);
+  // //Sanitize input
   // name = validator.escape(name);
   // email = validator.escape(email);
   // message = validator.escape(message);
-
-  // Validate input
-  if (!validator.isEmail(email)) {
-    res.status(400).send("Invalid email address");
-    return;
-  }
-  if (validator.isEmpty(message)) {
-    res.status(400).send("Message cannot be empty");
-    return;
-  }
-
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMPT_HOST,
-    port: process.env.SMPT_PORT,
-    auth: {
-      user: process.env.SMPT_USER,
-      pass: process.env.SMPT_PASSWORD,
-    },
-  });
-
-  try {
-    await transporter.sendMail({
-      from: email,
-      to: "moin@melvinrinkleff.de",
-      subject: `New message from ${name}`,
-      text: message,
-      replyTo: email,
-    });
-
-    res.status(200).send("Email sent successfully");
-  } catch (error) {
-    console.error(error);
-    res.status(500).send(error);
-  }
+  // // Validate input
+  // if (validator.isEmpty(email)) {
+  //   res.status(400).send("Email cannot be empty");
+  //   return;
+  // }
+  // if (validator.isEmpty(message)) {
+  //   res.status(400).send("Message cannot be empty");
+  //   return;
+  // }
+  // if (!validator.isEmail(email)) {
+  //   res.status(400).send("Invalid email address");
+  //   return;
+  // }
+  // const transporter = nodemailer.createTransport({
+  //   host: process.env.SMPT_HOST,
+  //   port: process.env.SMPT_PORT,
+  //   auth: {
+  //     user: process.env.SMPT_USER,
+  //     pass: process.env.SMPT_PASSWORD,
+  //   },
+  // });
+  // try {
+  //   await transporter.sendMail({
+  //     from: "moin@melvinrinkleff.de",
+  //     to: "moin@melvinrinkleff.de",
+  //     subject: `New message from ${name} (${email}))`,
+  //     text: message,
+  //     replyTo: email,
+  //   });
+  //   res.status(200).send("Email sent successfully");
+  // } catch (error) {
+  //   console.error(error);
+  //   res.status(500).send(error);
+  // }
 }
