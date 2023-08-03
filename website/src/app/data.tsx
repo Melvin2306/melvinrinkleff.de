506d31655f9cd807735f1b66126d83e307fcb819
Melvin2306 <melvin.rinkleff@gmx.de>
2023-08-03 18:40:34 +0200

    Create data.tsx

---
+++ website/src/app/data.tsx
@@ -0,0 +1,335 @@
+export default function dataprovider (params: Array) {
+    const data: {
+    name: "Melvin Rinkleff",
+    position: "Student Software Engineering",
+    email: "melvin.rinkleff@gmx.de",
+    phone: "+49 1590 1188669",
+    english: {
+    education: [
+      {
+        degree: "Exchange Semester",
+        institution: "NTNU - Norwegian University of Science and Technology",
+        period: "August 2023 - December 2023",
+        location: "Gjøvik, Norway",
+        courses: [
+        ],
+        url: "https://www.ntnu.edu",
+      },
+      {
+        degree: "Bachelor Software Engineering",
+        institution: "CODE University of Applied Sciences",
+        period: "2021 - Present",
+        location: "Berlin, Germany",
+        projects: [
+          "//Slash Hackathon",
+          "Building my own Web Application",
+          "Research in relationships and sexual pleasure",
+          "Spotify Data Analysis",
+          "Group Full Stack Development",
+        ],
+        url: "https://code.berlin",
+      },
+      {
+        degree:
+          "Bachelor Business Administration (Specialization Energy Economics)",
+        institution: "Hochschule Weserberland",
+        period: "2018 - 2021",
+        location: "Hameln, Germany",
+        project:
+          "Transformation of the German gas infrastructure to hydrogen infrastructure",
+        url: "https://www.hsw-hameln.de",
+      },
+    ],
+    experience: [
+      {
+        role: "Working Student Full Stack Development",
+        company: "Carbyte",
+        period: "August 2023 - Present",
+        location: "Remote",
+        description: "Fullstack development with Next.js",
+        url: "https://carbyte.de",
+      },
+      {
+        role: "Founder Associate (Working Student)",
+        company: "suena GmbH",
+        period: "September 2022 - July 2023",
+        location: "Hamburg, Germany (remote)",
+        description:
+          "Support of the founders in administrative tasks, business development and finance and development of KPIs",
+        url: "https://www.suena.energy",
+      },
+      {
+        role: "Working Student Operations",
+        company: "Ostrom",
+        period: "March 2022 - September 2022",
+        location: "Berlin, Germany",
+        description:
+          "Energy industry specific operations tasks, edifact communication with market partners, customer service",
+        url: "https://www.ostrom.de",
+      },
+      {
+        role: "Business Process Analyst",
+        company: "enercity",
+        period: "July 2021 - March 2022",
+        location: "Hannover, Germany (remote)",
+        description:
+          "Business process management and execution of business process automation for energy trading compliance processes",
+        url: "https://www.enercity.de",
+      },
+      {
+        role: "Dual Studies (Business Administration with Specialization in Energy Economics)",
+        company: "enercity",
+        period: "August 2018 - July 2021",
+        location: "Hannover, Germany",
+        description:
+          "Working in different teams and departments throughout the organization and special focus on the energy trading department and compliance",
+        url: "https://www.enercity.de",
+      },
+    ],
+    industryKnowledge: [
+      "Business Process Management",
+      "Full Stack Development",
+      "Project Management",
+      "Product Management",
+      "Energy Industry Research",
+    ],
+    skills: [
+      { name: "HTML/CSS" },
+      { name: "MS Office" },
+      { name: "Javascript" },
+      { name: "Python" },
+    ],
+    languages: [
+      { name: "German", proficiency: "Native" },
+      { name: "English", proficiency: "Fluent" },
+    ],
+    tools: [
+      { name: "Notion" },
+      { name: "Figma" },
+      { name: "Miro" },
+      { name: "VS Code" },
+      { name: "Intercom" },
+      { name: "Hubspot" },
+      { name: "SAP" },
+      { name: "Power Cloud" },
+      { name: "Postman" },
+      { name: "Trello" },
+      { name: "Jira" },
+      { name: "Asana" },
+      { name: "Slack" },
+    ],
+    socials: [
+      {
+        platform: "LinkedIn",
+        link: "https://linkedin.com/in/melvin-rinkleff",
+      },
+      {
+        platform: "Github",
+        link: "https://github.com/Melvin2306",
+      },
+    ],
+    additionalCourses: [
+      {
+        name: "Basic Certificate of Project Management",
+        institution: "GPM-Deutsche Gesellschaft für Projektmanagement e.V.",
+        year: "2020",
+        location: "Hameln, Germany",
+        url: "https://www.gpm-ipma.de",
+      },
+    ],
+    voluntaryWork: [
+      {
+        role: "Member of the Social Committee",
+        organization: "CHANCEN eG",
+        period: "2022 - Present",
+        location: "Berlin, Germany",
+        description:
+          "Deciding over requests for hardship from repayers. CHANCEN eG promotes equal opportunities for students irrespective of their financial background to finance their studies through an income share agreement",
+        url: "https://chancen-eg.de",
+      },
+      {
+        role: "Hackathon Event Management",
+        organization: "//Slash Berlin",
+        period: "2021 - Present",
+        location: "Berlin, Germany",
+        description:
+          "Event Management and various other tasks at //Slash. //Slash is a student initiative at CODE University at which we are organizing a sustainability hackathon for 400 students and young professionals",
+        url: "https://www.slash.berlin",
+      },
+      {
+        role: "Member of the Student Council",
+        organization: "CODE University of Applied Sciences",
+        period: "2021 - 2022",
+        location: "Berlin, Germany",
+        description:
+          "Representation of the student body in different areas of the university life at CODE, in particular being a member of the Admission Committee, initiating and organizing community events",
+        url: "https://code.berlin",
+      },
+    ],
+  };
+  german: {
+    education: [
+        {
+          degree: "Auslandssemester",
+          institution: "NTNU - Norwegian University of Science and Technology",
+          period: "August 2023 - Dezember 2023",
+          location: "Gjøvik, Norwegen",
+          courses: [
+          ],
+          url: "https://www.ntnu.edu",
+        },
+        {
+          degree: "Bachelor Software Engineering",
+          institution: "CODE University of Applied Sciences",
+          period: "2021 - Heute",
+          location: "Berlin, Deutschland",
+          projects: [
+            "//Slash Hackathon",
+            "Erstellen einer eigenen Web Applikation",
+            "Forschungsprojekt im Bereich von sexuellen Beziehungen und sexueller Befriedigung",
+            "Spotify Daten Analyse",
+            "Gruppen Projekt Full Stack Web Applikation",
+          ],
+          url: "https://code.berlin",
+        },
+        {
+          degree:
+            "Bachelor Betriebswirtschaftslehre (Fachrichtung Energiewirtschaft) ",
+          institution: "Hochschule Weserberland",
+          period: "2018 - 2021",
+          location: "Hameln, Deutschland",
+          project:
+            "Transformation der aktuellen Gas Infrastuktur in Deutschland zu einer Wasserstoff Infrastruktur",
+          url: "https://www.hsw-hameln.de",
+        },
+      ],
+      experience: [
+        {
+          role: "Working Student Full Stack Development",
+          company: "Carbyte",
+          period: "August 2023 - Present",
+          location: "Remote",
+          description: "Fullstack development with Next.js",
+          url: "https://carbyte.de",
+        },
+        {
+          role: "Founder Associate (Working Student)",
+          company: "suena GmbH",
+          period: "September 2022 - July 2023",
+          location: "Hamburg, Germany (remote)",
+          description:
+            "Support of the founders in administrative tasks, business development and finance and development of KPIs",
+          url: "https://www.suena.energy",
+        },
+        {
+          role: "Working Student Operations",
+          company: "Ostrom",
+          period: "March 2022 - September 2022",
+          location: "Berlin, Germany",
+          description:
+            "Energy industry specific operations tasks, edifact communication with market partners, customer service",
+          url: "https://www.ostrom.de",
+        },
+        {
+          role: "Business Process Analyst",
+          company: "enercity",
+          period: "July 2021 - March 2022",
+          location: "Hannover, Germany (remote)",
+          description:
+            "Business process management and execution of business process automation for energy trading compliance processes",
+          url: "https://www.enercity.de",
+        },
+        {
+          role: "Dual Studies (Business Administration with Specialization in Energy Economics)",
+          company: "enercity",
+          period: "August 2018 - July 2021",
+          location: "Hannover, Germany",
+          description:
+            "Working in different teams and departments throughout the organization and special focus on the energy trading department and compliance",
+          url: "https://www.enercity.de",
+        },
+      ],
+      industryKnowledge: [
+        "Business Process Management",
+        "Full Stack Development",
+        "Project Management",
+        "Product Management",
+        "Energy Industry Research",
+      ],
+      skills: [
+        { name: "HTML/CSS" },
+        { name: "MS Office" },
+        { name: "Javascript" },
+        { name: "Python" },
+      ],
+      languages: [
+        { name: "German", proficiency: "Native" },
+        { name: "English", proficiency: "Fluent" },
+      ],
+      tools: [
+        { name: "Notion" },
+        { name: "Figma" },
+        { name: "Miro" },
+        { name: "VS Code" },
+        { name: "Intercom" },
+        { name: "Hubspot" },
+        { name: "SAP" },
+        { name: "Power Cloud" },
+        { name: "Postman" },
+        { name: "Trello" },
+        { name: "Jira" },
+        { name: "Asana" },
+        { name: "Slack" },
+      ],
+      socials: [
+        {
+          platform: "LinkedIn",
+          link: "https://linkedin.com/in/melvin-rinkleff",
+        },
+        {
+          platform: "Github",
+          link: "https://github.com/Melvin2306",
+        },
+      ],
+      additionalCourses: [
+        {
+          name: "Basic Certificate of Project Management",
+          institution: "GPM-Deutsche Gesellschaft für Projektmanagement e.V.",
+          year: "2020",
+          location: "Hameln, Germany",
+          url: "https://www.gpm-ipma.de",
+        },
+      ],
+      voluntaryWork: [
+        {
+          role: "Member of the Social Committee",
+          organization: "CHANCEN eG",
+          period: "2022 - Present",
+          location: "Berlin, Germany",
+          description:
+            "Deciding over requests for hardship from repayers. CHANCEN eG promotes equal opportunities for students irrespective of their financial background to finance their studies through an income share agreement",
+          url: "https://chancen-eg.de",
+        },
+        {
+          role: "Hackathon Event Management",
+          organization: "//Slash Berlin",
+          period: "2021 - Present",
+          location: "Berlin, Germany",
+          description:
+            "Event Management and various other tasks at //Slash. //Slash is a student initiative at CODE University at which we are organizing a sustainability hackathon for 400 students and young professionals",
+          url: "https://www.slash.berlin",
+        },
+        {
+          role: "Member of the Student Council",
+          organization: "CODE University of Applied Sciences",
+          period: "2021 - 2022",
+          location: "Berlin, Germany",
+          description:
+            "Representation of the student body in different areas of the university life at CODE, in particular being a member of the Admission Committee, initiating and organizing community events",
+          url: "https://code.berlin",
+        },
+      ],
+    }
+}
+
+}
